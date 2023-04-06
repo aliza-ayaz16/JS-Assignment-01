8ce8b005cb3528d6bafc2118e1098c6fc55d9626
aliza-ayaz16 <syedaalizaayaz.1276@gmail.com>
2023-04-07 03:26:10 +0500

    first assignment completed

---
+++ assets/js/app.js
@@ -0,0 +1,133 @@
+// ====================================== ASSIGNMENT 01 ================================
+// ====================================== QUESTION 1 ================================
+// let num01 = +prompt("Enter first number:");
+// let num02 = +prompt("Enter second number:");
+
+// if (num01 > num02) {
+//  console.log(num01 + " is greater");
+// }
+
+// else if (num02 > num01) {
+//     console.log(num02 + " is greater");
+// }
+
+// else {
+//     console.log("Both numbers are equal");
+// }
+
+// ====================================== QUESTION 2 ================================
+// let num = +prompt("Enter a number:")
+// if (num > 0) {
+//     console.log("+ (positive)")
+// }
+
+// else if (num < 0){
+//     console.log("- (negative)")
+// }
+
+// else if (num === 0) {
+//     console.log("The number entered is zero")
+// }
+
+// ====================================== QUESTION 3 ================================
+// let num1 = +prompt("Enter the first number:");
+// let num2 = +prompt("Enter the second number:");
+// let num3 = +prompt("Enter the third number:");
+// let num4 = +prompt("Enter the fourth number:");
+// let num5 = +prompt("Enter the fifth number:");
+
+// if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+//     console.log(num1 + " is greatest ");
+// }
+
+// else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5) {
+//     console.log(num2 + " is greatest ");
+// }
+
+// else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5) {
+//     console.log(num3 + " is greatest ");
+// }
+
+// else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5) {
+//     console.log(num4 + " is greatest ");
+// }
+
+// else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4) {
+//     console.log(num5 + " is greatest ");
+// }
+
+// else {
+//     console.log(" No number is greatest as you have entered two equal numbers ");
+// }
+
+// ====================================== QUESTION 4 ================================
+// for (i=0; i<=15; i++) {
+//     if(i % 2 == 0){
+//         console.log(i + " is even")
+//     }
+
+//     else {
+//         console.log(i + " is odd")
+//     }
+// }
+
+// ====================================== QUESTION 5 ================================
+// let marks = prompt("Enter your marks:")
+// if (marks < 0) {
+//     console.log(" Invalid input, please enter a number between 1 & 100 (inclusive) ");
+// }
+
+// else if (marks < 60) {
+//     console.log(" F ");
+// }
+
+// else if (marks < 70) {
+//     console.log(" E ");
+// }
+
+// else if (marks < 80 ) {
+//     console.log(" C ");
+// }
+
+// else if (marks < 90 ) {
+//     console.log(" B ");
+// }
+
+// else if (marks <= 100 ) {
+//     console.log(" A ");
+// }
+
+// else if (marks > 100) {
+//     console.log(" Invalid input, please enter a number between 1 & 100 (inclusive) ");
+// }
+
+
+
+// ====================================== QUESTION 6 ================================
+// for (i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log(i + " FizzBuzz");
+//     }
+
+//     else if (i % 3 == 0) {
+//         console.log(i + " Fizz");
+//     }
+
+//     else if (i % 5 == 0) {
+//         console.log(i + " Buzz");
+//     }
+
+//     else {
+//         console.log(i)
+//     }
+// }
+
+// ====================================== QUESTION 7 ================================
+// for (i = 1; i <= 5; i++) {
+//     for (j = 1; j <= i; j++) {
+//         document.write("*");
+//     }
+
+//     document.write("<br>")
+// }
+
